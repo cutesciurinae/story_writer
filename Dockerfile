888eabbdebc02d089dbf1ae8c7e6c46bc57e0966
Mr. Saxy <jstaehle@uni-potsdam.de>
2026-02-11 20:26:17 +0100

    Change Port from 5000 to 4999

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ WORKDIR /app
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
-EXPOSE 5000
+EXPOSE 4999
 CMD ["python", "server.py"]
--- server.py
+++ server.py
@@ -205,5 +205,5 @@ def on_disconnect():
 
 
 if __name__ == '__main__':
-	socketio.run(app, host='0.0.0.0', port=5000)
+	socketio.run(app, host='0.0.0.0', port=4999, allow_unsafe_werkzeug=True)
 
