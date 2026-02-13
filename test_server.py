32503c0525bd7a2c2048be5409c5a3f974417e35
Mr. Saxy <jstaehle@uni-potsdam.de>
2026-02-13 23:41:08 +0100

    removed test_server, make a new ticket for unit tests.

--- test_server.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from server import app
-
-def test_index_route():
-    with app.test_client() as client:
-        response = client.get('/')
-        assert response.status_code == 200
-        assert b'<html' in response.data or b'<!DOCTYPE html' in response.data
